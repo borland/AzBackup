e6bc031cb8881650168241878deef51e08fa19fe
Orion Edwards <orion.edwards@gallagher.com>
2019-12-30 13:56:15 +1300

    Initial Commit

---
+++ AzBackup/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AzBackup
+//
+//  Created by Orion Edwards on 30/12/19.
+//  Copyright © 2019 orionedwards. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
